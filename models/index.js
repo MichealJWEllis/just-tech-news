fdd8e0b9effc384a5dd419afd60365b24c853f1d
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-05-24 16:27:13 -0400

    complete startover for understanding stopped at mod 13.4.1

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./Users');
+const Post = require("./Post");
+
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+module.exports = { User, Post };
